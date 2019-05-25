6b351813b917d410293cf966035dc6894e2a5b47
Angular2Guy <angular2guy@gmail.com>
2019-05-25 07:31:45 +0200

    Add License

--- src/main/java/ch/xxx/dbimporter/service/ImportService.java
+++ src/main/java/ch/xxx/dbimporter/service/ImportService.java
@@ -1,3 +1,15 @@
+/**
+ *    Copyright 2019 Sven Loesekann
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+       http://www.apache.org/licenses/LICENSE-2.0
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
 package ch.xxx.dbimporter.service;
 
 import java.io.File;
@@ -37,8 +49,8 @@ public class ImportService {
 	public String importFile(String fileName) {
 		LOG.info("ImportFile start");
 		LocalDateTime start = LocalDateTime.now();		
-		this.rowRepository.deleteAll();
-		LOG.info(String.format("ImportFile Db cleaned in %d sec",Duration.between(start,LocalDateTime.now()).getSeconds()));
+		this.rowRepository.bulkDelete();
+		LOG.info(String.format("ImportFile Db cleaned in %d sec",Duration.between(start,LocalDateTime.now()).getSeconds()));		
 		Path csvPath = Paths.get(this.tmpDir + "/" + fileName);
 		Flux<String> lineFlux = Flux.using(() -> Files.lines(csvPath), Flux::fromStream, BaseStream::close);
 		lineFlux.flatMap(str -> this.strToRow(str)).buffer(1000).parallel().runOn(Schedulers.parallel()).subscribe(rows -> this.storeRows(rows,start));
